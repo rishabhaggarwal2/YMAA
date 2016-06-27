c85686ada12fd5ca5fe630d3aab1a0db83e63b07
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2016-06-27 01:46:22 -0700

    Setting up website and home page with angular

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('ymaaSPA', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+});
