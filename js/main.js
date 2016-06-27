f37db43b3b3647b2c413a55e7bdb3b13cd95fdec
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2016-06-27 02:31:14 -0700

    Nav controller added and nav changed dynamically

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var app = angular.module('ymaaSPA', [
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     // else 404
@@ -24,16 +24,38 @@ app.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 /**
- * Controls the Blog
+ * Controls the Home
  */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
+app.controller('HomeCtrl', function ($scope/* $scope, $location, $http */) {
+  console.log("Home Controller reporting for duty.");
 });
 
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+app.controller('PageCtrl', function ($scope/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
+});
 
+/**
+ * Controls navBar
+ */
+app.controller('NavCtrl', function ($scope, $location/* $scope, $location, $http */) {
+  console.log("Nav Controller reporting for duty.");
+  $scope.nav = false;
+  if($location.url() != '/'){
+          $scope.nav = true;
+  }else{
+      $scope.nav = false;
+  }  
+  
+  $scope.$on('$locationChangeSuccess', function () {
+    console.log($location.url(), $scope.nav);
+    if($location.url() != '/'){
+            $scope.nav = true;
+    }else{
+        $scope.nav = false;
+    }  
+  });
+  
 });
