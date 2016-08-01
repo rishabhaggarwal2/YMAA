af921fb13ee29603dbfc5aeb455a3b630b9d848f
Luke Chui <chui.luke99@gmail.com>
2016-08-01 03:06:18 -0700

    mongo

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   AWS_ACCESS_KEY: 'test-key',
   AWS_SECRET_KEY: 'test-secret-key',
-  MONGOURL: process.env.MONGO_URI || 'mongodb://localhost:27017/YMAA',
+  MONGOURL: process.env.MONGO_URI,
+  // MONGOURL: process.env.MONGO_URI || 'mongodb://localhost:27017/YMAA',
   S3_BUCKET: 'name-of-bucket',
   STRIPE_KEY: 'test-key'
 }
