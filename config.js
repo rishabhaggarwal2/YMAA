7aefc8145d24e1532f12a95267f44821a2f1ef0c
Luke Chui <chui.luke99@gmail.com>
2016-08-16 16:29:00 -0700

    bucket name

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
   AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
   MONGOURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/YMAA',
-  S3_BUCKET: 'ymaa-content',
+  S3_BUCKET: process.env.S3_BUCKET,
   STRIPE_KEY: 'test-key'
 }
