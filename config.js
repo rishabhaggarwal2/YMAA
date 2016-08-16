d8e24758ef7895bd345129bbc97b2e265b54d8ed
Luke Chui <chui.luke99@gmail.com>
2016-08-16 16:25:36 -0700

    added aws keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  AWS_ACCESS_KEY: 'test-key',
-  AWS_SECRET_KEY: 'test-secret-key',
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
   MONGOURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/YMAA',
-  S3_BUCKET: 'name-of-bucket',
+  S3_BUCKET: 'ymaa-content',
   STRIPE_KEY: 'test-key'
 }
